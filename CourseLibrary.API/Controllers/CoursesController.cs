c6280dcffef01d5441d14cbca1298a3ea3293c39
Dave <ddawid@windowslive.com>
2019-11-28 22:23:29 +0100

    Update CoursesController.cs
    Add get methods to CourseController

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -2,14 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseLibrary.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/authors/{authorId}/courses")]
     [ApiController]
     public class CoursesController : ControllerBase
     {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
+        {
+            if (!courseLibraryRepository.AuthorExists(authorId)){
+                return NotFound();
+            }
+            var coursesFromRepository = courseLibraryRepository.GetCourses(authorId);
+            return Ok(mapper.Map<IEnumerable<CourseDto>>(coursesFromRepository));
+        }
+
+        [HttpGet("{courseId}")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!courseLibraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var courseFromRepo = courseLibraryRepository.GetCourse(authorId, courseId);
+            if (courseFromRepo == null)
+                return NotFound();
+
+            return Ok(mapper.Map<CourseDto>(courseFromRepo));
+        }
     }
 }
